e2de039a86bcf420e2f38bb362c45fbe1e0df8cb
yeggrrr <yjkim981103@gmail.com>
2024-07-11 16:54:53 +0900

    [Feat] SearchVC & SearchTableViewCell UI

---
+++ YegrWeatherEX/Search/SearchTableViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  SearchTableViewCell.swift
+//  YegrWeatherEX
+//
+//  Created by YJ on 7/11/24.
+//
+
+import UIKit
+import SnapKit
+
+class SearchTableViewCell: BaseTableViewCell {
+    let nameStackView = UIStackView()
+    let regionLabel = UILabel()
+    let countryLabel = UILabel()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(nameStackView)
+        nameStackView.addArrangedSubview(regionLabel)
+        nameStackView.addArrangedSubview(countryLabel)
+    }
+    
+    override func configureLayout() {
+        let safeArea = contentView.safeAreaLayoutGuide
+        let height = contentView.frame.height
+        
+        nameStackView.snp.makeConstraints {
+            $0.edges.equalTo(contentView.snp.edges)
+        }
+        
+        regionLabel.snp.makeConstraints {
+            $0.height.equalTo(height / 2)
+        }
+        
+        countryLabel.snp.makeConstraints {
+            $0.height.equalTo(height / 2)
+        }
+            
+        
+    }
+    
+    override func configureUI() {
+        nameStackView.axis = .vertical
+        nameStackView.spacing = 0
+        nameStackView.distribution = .fillEqually
+        
+        regionLabel.text = "지역이름"
+        countryLabel.text = "나라이름"
+        regionLabel.setUI(txtColor: .white, txtAlignment: .left, fontStyle: .systemFont(ofSize: 17, weight: .semibold))
+        countryLabel.setUI(txtColor: .white, txtAlignment: .left, fontStyle: .systemFont(ofSize: 15, weight: .regular))
+    }
+    
+}
