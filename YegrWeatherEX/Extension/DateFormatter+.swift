f0993355bb26d5b759cf4e28453302edd000d394
yeggrrr <yjkim981103@gmail.com>
2024-07-13 03:11:42 +0900

    [Feat] DateFormatter 추가

--- YegrWeatherEX/Extension/DateFormatter+.swift
+++ YegrWeatherEX/Extension/DateFormatter+.swift
@@ -15,10 +15,36 @@ extension DateFormatter {
         return dateFormat
     }()
     
-    static let OnlyHourDateFormatter: DateFormatter = {
+    static let onlyHourDateFormatter: DateFormatter = {
         let dateFormat = DateFormatter()
         dateFormat.locale = Locale(identifier: "ko_KR")
         dateFormat.dateFormat = "HH시"
         return dateFormat
     }()
+    
+    static let stringToDateFormatter: DateFormatter = {
+        let dateFormat = DateFormatter()
+        dateFormat.locale = Locale(identifier: "ko_KR")
+        dateFormat.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormat.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+        return dateFormat
+    }()
+    
+    static let stringToOnlyDateFormatter: DateFormatter = {
+        let dateFormat = DateFormatter()
+        dateFormat.locale = Locale(identifier: "ko_KR")
+        dateFormat.dateFormat = "yyyyMMdd"
+        dateFormat.timeZone = NSTimeZone(name: "UTC") as TimeZone?
+        return dateFormat
+    }()
+    
+    static func longToShortDate(dateString: String) -> String {
+        guard let date = DateFormatter.stringToDateFormatter.date(from: dateString) else { return "-" }
+        return DateFormatter.onlyDateFormatter.string(from: date)
+    }
+    
+    static func longToOnlyHour(dateString: String) -> String {
+        guard let date = DateFormatter.stringToDateFormatter.date(from: dateString) else { return "-" }
+        return DateFormatter.onlyHourDateFormatter.string(from: date)
+    }
 }
