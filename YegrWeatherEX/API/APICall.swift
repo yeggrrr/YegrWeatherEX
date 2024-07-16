fe9c4842cb556fd79156fc99b6fa17f36deac181
yeggrrr <yjkim981103@gmail.com>
2024-07-17 00:35:48 +0900

    [Feat] Result Type을 통한 APICall 코드 개선

--- YegrWeatherEX/API/APICall.swift
+++ YegrWeatherEX/API/APICall.swift
@@ -10,18 +10,51 @@ import Alamofire
 
 class APICall {
     static let shared = APICall()
-    
     private init() { }
     
-    func callRequest<T:Decodable>(api: APIRequest, model: T.Type, completion: @escaping (T?) -> Void, errorHandler: @escaping (String) -> Void) {
-        AF.request(api.endpoint, method: api.method, parameters: api.parameter).responseDecodable(of: T.self) { response in
-            switch response.result {
-            case .success(let value):
-                completion(value)
-            case .failure(let error):
-                errorHandler(error.localizedDescription)
-                print(error)
+    // func callRequest<T:Decodable>(api: APIRequest, model: T.Type, completion: @escaping (T?) -> Void, errorHandler: @escaping (String) -> Void) {
+    //     AF.request(api.endpoint, method: api.method, parameters: api.parameter).responseDecodable(of: T.self) { response in
+    //         switch response.result {
+    //         case .success(let value):
+    //             completion(value)
+    //         case .failure(let error):
+    //             errorHandler(error.localizedDescription)
+    //             print(error)
+    //         }
+    //     }
+    // }
+    
+    func callRequest<T:Decodable>(api: APIRequest, model: T.Type, completion: @escaping (Result<T, Error>) -> Void) {
+        guard var urlComponents = URLComponents(string: api.endpoint) else { return }
+        let queryItemArray = api.parameters.map {
+            URLQueryItem(name: $0.key, value: $0.value)
+        }
+        
+        urlComponents.queryItems = queryItemArray
+        
+        guard let requestURL = urlComponents.url else { return }
+    
+        URLSession.shared.dataTask(with: requestURL) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(result.self))
+                }
+            } catch {
+                print(error.localizedDescription)
+                completion(.failure(error))
             }
         }
+        .resume()
     }
 }
--- YegrWeatherEX/API/APIRequest.swift
+++ YegrWeatherEX/API/APIRequest.swift
@@ -16,11 +16,38 @@ enum APIRequest {
         return "https://api.openweathermap.org/data/2.5/"
     }
     
+    var endpoint: String {
+        switch self {
+        case .current(_):
+            return baseURL + "weather"
+        case .threeHours(_):
+            return baseURL + "forecast"
+        }
+    }
+    
+    var parameters: [String: String] {
+        switch self {
+        case .current(let id):
+            return ["appid": APIKey.weatherKey, "id": "\(id)", "lang": "kr", "units": "metric"]
+        case .threeHours(let id):
+            return ["appid": APIKey.weatherKey, "id": "\(id)", "lang": "kr", "units": "metric"]
+        }
+    }
+}
+
+enum AFAPIRequest {
+    case current(id: Int)
+    case threeHours(id: Int)
+    
+    var baseURL: String {
+        return "https://api.openweathermap.org/data/2.5/"
+    }
+    
     var endpoint: URL {
         switch self {
-        case .current(id: let id):
+        case .current(let id):
             return URL(string: baseURL + "weather?id=\(id)")!
-        case .threeHours(id: let id):
+        case .threeHours(let id):
             return URL(string: baseURL + "forecast?id=\(id)")!
         }
     }
