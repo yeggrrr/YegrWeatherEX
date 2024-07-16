93d64da60177f8ba6ede06fa70676a2340b77f42
yeggrrr <yjkim981103@gmail.com>
2024-07-16 18:21:40 +0900

    [Delete] 불필요한 코드 삭제

--- YegrWeatherEX/API/APICall.swift
+++ YegrWeatherEX/API/APICall.swift
@@ -24,16 +24,4 @@ class APICall {
             }
         }
     }
-    
-    func callRequestArrayData<T:Decodable>(api: APIRequest, model: T.Type, completion: @escaping ([T]?) -> Void, errorHandler: @escaping (String) -> Void) {
-        AF.request(api.endpoint, method: api.method, parameters: api.parameter).responseDecodable(of: T.self) { response in
-            switch response.result {
-            case .success(let value):
-                completion([value])
-            case .failure(let error):
-                errorHandler(error.localizedDescription)
-                print(error)
-            }
-        }
-    }
 }
